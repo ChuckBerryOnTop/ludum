370a6d0b79d080ee1dbb1ccb02264819f9d1fa48
Daniel Kubiak <dan.kubiak@gmail.com>
2018-07-25 20:00:42 -0700

    fix

--- server.js
+++ server.js
@@ -19,11 +19,11 @@ app.use(morgan());
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/loginList");
-
+/*
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
-
+*/
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
